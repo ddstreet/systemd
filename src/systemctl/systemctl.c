1095b674c68ff2a24c2d692158fd5b974b2ae56a
Michael Biebl <biebl@debian.org>
2014-07-16 00:44:15 +0200

    change the log_info() line in systemctl to reflect reality
    Gbp-Pq: change-the-log_info-line-in-systemctl-to-reflect-rea.patch.

--- src/systemctl/systemctl.c
+++ src/systemctl/systemctl.c
@@ -4300,7 +4300,7 @@ static int enable_sysv_units(const char *verb, char **args) {
                 /* Mark this entry, so that we don't try enabling it as native unit */
                 args[f] = (char*) "";
 
-                log_info("%s is not a native service, redirecting to /usr/sbin/update-rc.d.", name);
+                log_info("Synchronizing state for %s with sysvinit using update-rc.d...", name);
 
                 argv[c++] = path_get_file_name(p);
                 argv[c++] = streq(verb, "enable") ? "defaults" : "remove";
