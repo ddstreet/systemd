f832c296a5bbc4f2bf2a78590b29d36ef5faf5b8
Martin Pitt <mpitt@debian.org>
2014-12-11 18:06:54 +0200

    delta: diff returns 1 when files differ, ignore this
    Gbp-Pq: delta-diff-returns-1-when-files-differ-ignore-this.patch.

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -2778,7 +2778,7 @@ static int change_uid_gid(char **_home) {
 
         truncate_nl(line);
 
-        wait_for_terminate_and_warn("getent passwd", pid);
+        wait_for_terminate_and_warn("getent passwd", pid, true);
 
         x = strchr(line, ':');
         if (!x) {
@@ -2862,7 +2862,7 @@ static int change_uid_gid(char **_home) {
 
         truncate_nl(line);
 
-        wait_for_terminate_and_warn("getent initgroups", pid);
+        wait_for_terminate_and_warn("getent initgroups", pid, true);
 
         /* Skip over the username and subsequent separator whitespace */
         x = line;
