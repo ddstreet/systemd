52d1306a6bef88977278e015cb16016b50320382
Michael Biebl <biebl@debian.org>
2015-11-16 18:08:32 +0100

    sd-dhcp-client: make request broadcasts configurable
    Gbp-Pq: sd-dhcp-client-make-request-broadcasts-configurable.patch.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -91,6 +91,7 @@ static int network_load_one(Manager *manager, const char *filename) {
         network->dhcp_domainname = true;
         network->dhcp_routes = true;
         network->dhcp_sendhost = true;
+        network->dhcp_broadcast = true;
 
         r = config_parse(NULL, filename, file, "Match\0Network\0Address\0Route\0DHCP\0DHCPv4\0", config_item_perf_lookup,
                         (void*) network_network_gperf_lookup, false, false, network);
