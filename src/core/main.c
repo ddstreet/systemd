055abf0ca094c95f9ecdedc526a334bf1192e96e
Steve Langasek <steve.langasek@ubuntu.com>
2017-01-13 13:41:23 +0200

    Revert "core: enable TasksMax= for all services by default,
    Gbp-Pq: Revert-core-enable-TasksMax-for-all-services-by-default-a.patch.

--- src/core/main.c
+++ src/core/main.c
@@ -123,8 +123,8 @@ static FILE* arg_serialization = NULL;
 static bool arg_default_cpu_accounting = false;
 static bool arg_default_blockio_accounting = false;
 static bool arg_default_memory_accounting = false;
-static bool arg_default_tasks_accounting = true;
-static uint64_t arg_default_tasks_max = UINT64_C(512);
+static bool arg_default_tasks_accounting = false;
+static uint64_t arg_default_tasks_max = (uint64_t) -1;
 static sd_id128_t arg_machine_id = {};
 
 static void pager_open_if_enabled(void) {
