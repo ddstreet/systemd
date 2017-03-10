1a70fe5723d88ee76081986d4ccf12cf8ecce10c
Michael Biebl <biebl@debian.org>
2017-03-10 06:02:49 +0100

    Add targets for compatibility with Debian insserv system facilities
    Gbp-Pq: Add-targets-for-compatibility-with-Debian-insserv-sy.patch.

--- src/sysv-generator/sysv-generator.c
+++ src/sysv-generator/sysv-generator.c
@@ -277,6 +277,9 @@ static int sysv_translate_facility(const char *name, const char *filename, char 
                 "remote_fs",            SPECIAL_REMOTE_FS_TARGET,
                 "syslog",               NULL,
                 "time",                 SPECIAL_TIME_SYNC_TARGET,
+                /* Debian defined facilities */
+                "x-display-manager",    "display-manager.service",
+                "mail-transport-agent", "mail-transport-agent.target",
         };
 
         unsigned i;
