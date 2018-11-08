f122885a00170e99546f704b9370c69c78b80941
Chris Coulson <chris.coulson@canonical.com>
2018-11-08 00:01:30 +0000

    [PATCH] resolved: fix loop on packets with pseudo dns types
    Gbp-Pq: CVE-2017-15908.patch.

--- src/resolve/resolved-dns-packet.c
+++ src/resolve/resolved-dns-packet.c
@@ -1449,7 +1449,7 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
 
                 found = true;
 
-                while (bitmask) {
+                for (; bitmask; bit++, bitmask >>= 1)
                         if (bitmap[i] & bitmask) {
                                 uint16_t n;
 
@@ -1463,10 +1463,6 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
                                 if (r < 0)
                                         goto fail;
                         }
-
-                        bit ++;
-                        bitmask >>= 1;
-                }
         }
 
         if (!found)
