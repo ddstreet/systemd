6f5a25023de283dc9f5bc60c602013a5d217c6cc
Dan Streetman <ddstreet@canonical.com>
2019-01-29 14:19:39 -0500

    [PATCH 11/11] journal-remote: set a limit on the number of fields in
    Gbp-Pq: CVE-2018-16865_2.patch.

--- src/journal-remote/journal-remote.c
+++ src/journal-remote/journal-remote.c
@@ -408,6 +408,9 @@ int journal_remote_handle_raw_source(
                 log_debug("%zu active sources remaining", s->active);
                 return 0;
         } else if (r == -E2BIG) {
+                log_notice("Entry with too many fields, skipped");
+                return 1;
+        } else if (r == -ENOBUFS) {
                 log_notice_errno(E2BIG, "Entry too big, skipped");
                 return 1;
         } else if (r == -EAGAIN) {
