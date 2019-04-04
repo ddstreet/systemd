a1e0218e1db96d100787cbcf1937d56e647a3c0c
Dan Streetman <ddstreet@canonical.com>
2019-04-04 07:29:38 -0400

    [PATCH] journal: fix syslog_parse_identifier()
    Gbp-Pq: CVE-2018-16866.patch.

--- src/journal/test-journal-syslog.c
+++ src/journal/test-journal-syslog.c
@@ -6,8 +6,8 @@
 #include "string-util.h"
 #include "syslog-util.h"
 
-static void test_syslog_parse_identifier(const char* str,
-                                         const char *ident, const char*pid, int ret) {
+static void test_syslog_parse_identifier(const char *str,
+                                         const char *ident, const char *pid, int ret) {
         const char *buf = str;
         _cleanup_free_ char *ident2 = NULL, *pid2 = NULL;
         int ret2;
@@ -33,7 +33,13 @@ static void test_syslog_parse_priority(const char *str, int priority, int ret) {
 int main(void) {
         test_syslog_parse_identifier("pidu[111]: xxx", "pidu", "111", 11);
         test_syslog_parse_identifier("pidu: xxx", "pidu", NULL, 6);
+        test_syslog_parse_identifier("pidu:  xxx", "pidu", NULL, 7);
         test_syslog_parse_identifier("pidu xxx", NULL, NULL, 0);
+        test_syslog_parse_identifier(":", "", NULL, 1);
+        test_syslog_parse_identifier(":  ", "", NULL, 3);
+        test_syslog_parse_identifier("pidu:", "pidu", NULL, 5);
+        test_syslog_parse_identifier("pidu: ", "pidu", NULL, 6);
+        test_syslog_parse_identifier("pidu : ", NULL, NULL, 0);
 
         test_syslog_parse_priority("<>", 0, 0);
         test_syslog_parse_priority("<>aaa", 0, 0);
