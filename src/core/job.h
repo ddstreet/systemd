2b96920e1f9938813228aeb8f6489e13ca6879ad
Balint Reczey <rbalint@ubuntu.com>
2019-09-30 17:23:17 +0200

    Backport daemon reload race fix (PR#11121)
    Gbp-Pq: fix-race-daemon-reload-11121.patch.

--- src/core/job.h
+++ src/core/job.h
@@ -178,7 +178,8 @@ struct Job {
 
 Job* job_new(Unit *unit, JobType type);
 Job* job_new_raw(Unit *unit);
-void job_free(Job *job);
+void job_unlink(Job *job);
+Job* job_free(Job *job);
 Job* job_install(Job *j);
 int job_install_deserialized(Job *j);
 void job_uninstall(Job *j);
@@ -238,6 +239,8 @@ void job_add_to_gc_queue(Job *j);
 
 int job_get_before(Job *j, Job*** ret);
 int job_get_after(Job *j, Job*** ret);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(Job*, job_free);
 
 const char* job_type_to_string(JobType t) _const_;
 JobType job_type_from_string(const char *s) _pure_;
