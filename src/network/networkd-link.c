ca8be117da839e57f95bd2a67125b814ba1d923e
Balint Reczey <rbalint@ubuntu.com>
2019-10-22 18:22:52 +0200

    Revert "network: prevent interfaces to be initialized multiple
    Gbp-Pq: Revert-network-prevent-interfaces-to-be-initialized-multi.patch.

--- src/network/networkd-link.c
+++ src/network/networkd-link.c
@@ -1234,7 +1234,7 @@ static int set_mtu_handler(sd_netlink *rtnl, sd_netlink_message *m, Link *link) 
         else
                 log_link_debug(link, "Setting MTU done.");
 
-        if (link->state == LINK_STATE_INITIALIZED) {
+        if (link->state == LINK_STATE_PENDING) {
                 r = link_configure_after_setting_mtu(link);
                 if (r < 0)
                         link_enter_failed(link);
@@ -2070,7 +2070,7 @@ static int link_enter_join_netdev(Link *link) {
 
         assert(link);
         assert(link->network);
-        assert(link->state == LINK_STATE_INITIALIZED);
+        assert(link->state == LINK_STATE_PENDING);
 
         link_set_state(link, LINK_STATE_CONFIGURING);
 
@@ -2518,7 +2518,7 @@ static int link_configure(Link *link) {
 
         assert(link);
         assert(link->network);
-        assert(link->state == LINK_STATE_INITIALIZED);
+        assert(link->state == LINK_STATE_PENDING);
 
         if (link->iftype == ARPHRD_CAN)
                 return link_configure_can(link);
@@ -2643,7 +2643,7 @@ static int link_configure_after_setting_mtu(Link *link) {
 
         assert(link);
         assert(link->network);
-        assert(link->state == LINK_STATE_INITIALIZED);
+        assert(link->state == LINK_STATE_PENDING);
 
         if (link->setting_mtu)
                 return 0;
@@ -2800,11 +2800,10 @@ static int link_initialized_and_synced(Link *link) {
 
         /* We may get called either from the asynchronous netlink callback,
          * or directly for link_add() if running in a container. See link_add(). */
-        if (!IN_SET(link->state, LINK_STATE_PENDING, LINK_STATE_INITIALIZED))
+        if (link->state != LINK_STATE_PENDING)
                 return 0;
 
         log_link_debug(link, "Link state is up-to-date");
-        link_set_state(link, LINK_STATE_INITIALIZED);
 
         r = link_new_bound_by_list(link);
         if (r < 0)
@@ -2884,7 +2883,6 @@ int link_initialized(Link *link, sd_device *device) {
                 return 0;
 
         log_link_debug(link, "udev initialized link");
-        link_set_state(link, LINK_STATE_INITIALIZED);
 
         link->sd_device = sd_device_ref(device);
 
@@ -3847,7 +3845,6 @@ void link_clean(Link *link) {
 
 static const char* const link_state_table[_LINK_STATE_MAX] = {
         [LINK_STATE_PENDING] = "pending",
-        [LINK_STATE_INITIALIZED] = "initialized",
         [LINK_STATE_CONFIGURING] = "configuring",
         [LINK_STATE_CONFIGURED] = "configured",
         [LINK_STATE_UNMANAGED] = "unmanaged",
