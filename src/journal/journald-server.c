b57d6bb7a8e21bb9c297d00633245fdabf1fcddb
Michael Biebl <biebl@debian.org>
2019-11-15 22:01:17 +0100

    Re-enable journal forwarding to syslog
    Gbp-Pq: debian/Re-enable-journal-forwarding-to-syslog.patch.

--- src/journal/journald-server.c
+++ src/journal/journald-server.c
@@ -2023,6 +2023,7 @@ int server_init(Server *s) {
                 .rate_limit_interval = DEFAULT_RATE_LIMIT_INTERVAL,
                 .rate_limit_burst = DEFAULT_RATE_LIMIT_BURST,
 
+                .forward_to_syslog = true,
                 .forward_to_wall = true,
 
                 .max_file_usec = DEFAULT_MAX_FILE_USEC,
