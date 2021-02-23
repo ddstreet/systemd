e15cdab622998107e09354c3bc24fb4be236c5fe
Ioanna Alifieraki <ioanna-maria.alifieraki@canonical.com>
2021-02-23 03:45:01 +0200

    [PATCH] networkd: Allow to retain configs even if carrier is lost
    Gbp-Pq: lp1860926/0001-networkd-Allow-to-retain-configs-even-if-carrier-is-.patch.

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -226,6 +226,7 @@ struct Network {
         int arp;
         bool unmanaged;
         bool configure_without_carrier;
+        bool ignore_carrier_loss;
         uint32_t iaid;
         DUID duid;
 
