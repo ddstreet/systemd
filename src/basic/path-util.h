9ed0ed7e8eaf90b4bb45181f92bc35988ef4626e
Dan Streetman <ddstreet@canonical.com>
2021-04-01 22:51:58 -0400

    Append /snap/bin to default PATH
    Gbp-Pq: Append-snap-bin-to-default-PATH.patch.

--- src/basic/path-util.h
+++ src/basic/path-util.h
@@ -31,9 +31,9 @@
 #define DEFAULT_PATH_SPLIT_USR DEFAULT_PATH_NORMAL ":/sbin:/bin"
 
 #ifdef HAVE_SPLIT_USR
-#  define DEFAULT_PATH DEFAULT_PATH_SPLIT_USR
+#  define DEFAULT_PATH DEFAULT_PATH_SPLIT_USR ":/snap/bin"
 #else
-#  define DEFAULT_PATH DEFAULT_PATH_NORMAL
+#  define DEFAULT_PATH DEFAULT_PATH_NORMAL ":/snap/bin"
 #endif
 
 bool is_path(const char *p) _pure_;
