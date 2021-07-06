3771dcee8cc7215eec7e56df92eb1fead9b35345
Dan Streetman <ddstreet@canonical.com>
2021-07-06 14:33:09 -0400

    [PATCH] fs-util: rename conservative_rename() ->
    Gbp-Pq: lp1891215/0003-fs-util-rename-conservative_rename-conservative_rena.patch.

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -123,4 +123,7 @@ int syncfs_path(int atfd, const char *path);
 
 int open_parent(const char *path, int flags, mode_t mode);
 
-int conservative_rename(int olddirfd, const char *oldpath, int newdirfd, const char *newpath);
+int conservative_renameat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath);
+static inline int conservative_rename(const char *oldpath, const char *newpath) {
+        return conservative_renameat(AT_FDCWD, oldpath, AT_FDCWD, newpath);
+}
