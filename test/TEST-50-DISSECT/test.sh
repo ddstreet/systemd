95b4ec9777d1e83d484d41efa8879413a49cfb62
Nick Rosbrook <nick.rosbrook@canonical.com>
2023-02-14 11:52:31 -0500

    test: increase QEMU_MEM for some tests
    Gbp-Pq: test-increase-QEMU_MEM-for-some-tests.patch.

--- test/TEST-50-DISSECT/test.sh
+++ test/TEST-50-DISSECT/test.sh
@@ -12,6 +12,7 @@ TEST_INSTALL_VERITY_MINIMAL=1
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+QEMU_MEM="1024M"
 
 command -v mksquashfs >/dev/null 2>&1 || exit 0
 command -v veritysetup >/dev/null 2>&1 || exit 0
